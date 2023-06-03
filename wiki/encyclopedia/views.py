917dd3a82988ead03d09c35c7c8014128613b72d
Basith Mohammed <basithm0501@gmail.com>
2023-06-03 12:19:58 -0400

    finished search functionality

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -23,6 +23,21 @@ def entry(request, topic):
         })
 
 def result(request):
-    return render(request, "encyclopedia/results.html", {
-        "entries": util.list_entries()
-    })
+    search = request.GET.get('q', '')
+    if search in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "entries": util.list_entries(),
+            "topic": search,
+            "content": markdown2.markdown(util.get_entry(search)),
+        })
+    else:
+        relatedresults = []
+        for entry in util.list_entries():
+            if search in entry:
+                relatedresults.append(entry)
+
+        return render(request, "encyclopedia/results.html", {
+            "entries": util.list_entries(),
+            "results": relatedresults
+        })
+        
